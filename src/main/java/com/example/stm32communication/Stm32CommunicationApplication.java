fb1604ee78557a3fe9e368506e7c16de7cbae36d
nzx3A1 <1901771781@qq.com>
2022-09-16 14:49:16 +0800

    test1

---
+++ src/main/java/com/example/stm32communication/Stm32CommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.stm32communication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stm32CommunicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stm32CommunicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stm32communication/Stm32CommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stm32communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stm32CommunicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
